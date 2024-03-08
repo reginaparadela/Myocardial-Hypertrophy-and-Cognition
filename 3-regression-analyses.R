dbdf17dd53c244d4c4378bd07a9908501e6f1c28
Regina Paradela <123121459+rparadela@users.noreply.github.com>
2024-03-07 20:21:40 -0800

    3-regression-analyses.R

--- 3-regression-analyses.R
+++ 3-regression-analyses.R
@@ -257,7 +257,7 @@ exp(cbind(OR = coef(m3), confint(m3)))
 
 df_lvh$age_cat <-  ifelse(df_lvh$age >= 75, 1, 0)
 df_lvh$age_cat <- as.factor(df_lvh$age_cat)
-
+levels(df_lvh$age_cat) <- c('<75 y', '>75 y')
 
 m3 <- lm(cdr_sb ~ left_ventr_wall_thickness*age_cat
          + education_deceased
@@ -273,8 +273,53 @@ m3 <- lm(cdr_sb ~ left_ventr_wall_thickness*age_cat
 summary(m3)
 hist(m3$residuals)
 confint(m3)
+car::Anova(m3, type = "III")# to get the overall p for the interaction
+
+#1. Plotting with sjPlot
+
+#to customize the plot appearance 
+set_theme(base = theme_classic() + theme(panel.grid.major.y = element_line()),
+          title.align = "center",
+          title.size = 1.5, 
+          axis.title.size = 1.2, 
+          axis.textsize.x = 1, 
+          axis.textsize.y = 1)
+
+
+plot_model(m3, type = "pred", terms = c("left_ventr_wall_thickness", "age_cat"),
+           axis.title = c("Left Ventricular Wall Thickness", "CDR-SB"), 
+           title = "Predicted values of CDR-SB",  
+           legend.title = " ", 
+           ci.lvl = NA, #ci.lvl = NA removes confidence interval (default is 89%)
+           )
+
+#2. Plotting the interaction with ggplot
+
+if(!require("ggeffects")) install.packages("ggeffects")
+
+df <- ggpredict(m3, terms = c("left_ventr_wall_thickness", "age_cat"))
+p1 <- 
+  ggplot(df, aes(x, predicted)) +
+  theme_classic()+
+  #theme(panel.grid.major.y = element_line()) + #to add horizontal lines on the background
+  geom_line(aes(linetype=group, color=group)) + 
+  geom_ribbon(aes(ymin=conf.low, ymax=conf.high, fill=group), alpha=0.15)+ #confidence interval
+  scale_linetype_manual(values = c("solid", "solid")) +
+  scale_y_continuous(limits = c(0,5)) + 
+  theme(legend.title = element_blank(), 
+        axis.title = element_text(size = 14),
+        plot.title = element_text(size = 14, vjust = +2),
+        axis.title.x = element_text(size = 12), 
+        axis.title.y = element_text(size = 12),) +
+  xlab("Left Ventricular Wall Thickness")+
+  ylab("Predicted values of CDR-SB") +
+  ggtitle("A)")
+
+p1
 
 # Sex 
+
+levels(df_lvh$deceased_sex)
 
 m3 <- lm(cdr_sb ~ left_ventr_wall_thickness*deceased_sex
          + age
@@ -290,8 +335,44 @@ m3 <- lm(cdr_sb ~ left_ventr_wall_thickness*deceased_sex
 summary(m3)
 hist(m3$residuals)
 confint(m3)
+car::Anova(m3, type = "III")
+
+plot_model(m3, type = "pred", terms = c("left_ventr_wall_thickness", "deceased_sex"), 
+           axis.title = c("Left Ventricular Wall Thickness", "CDR-SB"), 
+           title = "Predicted values of CDR-SB",  
+           legend.title = " ", 
+           ci.lvl = NA, #ci.lvl = confidence interval - default is 89%
+           )
+
+#2. Plotting the interaction with ggplot
+
+if(!require("ggeffects")) install.packages("ggeffects")
+
+df <- ggpredict(m3, terms = c("left_ventr_wall_thickness", "deceased_sex"))
+p2 <- 
+  ggplot(df, aes(x, predicted)) +
+  theme_classic()+
+  #theme(panel.grid.major.y = element_line()) + #to add horizontal lines on the background
+  geom_line(aes(linetype=group, color=group)) + 
+  geom_ribbon(aes(ymin=conf.low, ymax=conf.high, fill=group), alpha=0.15)+ #confidence interval
+  scale_linetype_manual(values = c("solid", "solid")) +
+  scale_y_continuous(limits = c(0,5)) + 
+  theme(legend.title = element_blank(), 
+        axis.title = element_text(size = 14),
+        plot.title = element_text(size = 14, vjust = +2),
+        axis.title.x = element_text(size = 12), 
+        axis.title.y = element_text(size = 12),) +
+  xlab("Left Ventricular Wall Thickness")+
+  ylab("Predicted values of CDR-SB") +
+  ggtitle("B)")
+
+p2
 
 # Race  
+
+levels(df_lvh$race)
+df_lvh_noasian <- df_lvh %>% dplyr::filter(!race == "Asian")
+levels(df_lvh_noasian$race) <- c("White","Black", NA)
 
 m3 <- lm(cdr_sb ~ left_ventr_wall_thickness*race
          + age
@@ -303,14 +384,50 @@ m3 <- lm(cdr_sb ~ left_ventr_wall_thickness*race
          + bmi
          + smoking_yn
          + alcohol_yn
-         + physical_activity, data = df_lvh)
+         + physical_activity, data = df_lvh_noasian)
 summary(m3)
 hist(m3$residuals)
 confint(m3)
+car::Anova(m3, type = "III")
+
+plot_model(m3, type = "pred", terms = c("left_ventr_wall_thickness", "race"), 
+           axis.title = c("Left Ventricular Wall Thickness", "CDR-SB"), 
+           title = "Predicted values of CDR-SB",  
+           legend.title = " ", 
+           ci.lvl = NA, #ci.lvl = confidence interval - default is 89%
+)
+
+
+#2. Plotting the interaction with ggplot
+
+if(!require("ggeffects")) install.packages("ggeffects")
+
+df <- ggpredict(m3, terms = c("left_ventr_wall_thickness", "race"))
+
+p3 <- 
+  ggplot(df, aes(x, predicted)) +
+  theme_classic()+
+  #theme(panel.grid.major.y = element_line()) + #to add horizontal lines on the background
+  geom_line(aes(linetype=group, color=group)) + 
+  geom_ribbon(aes(ymin=conf.low, ymax=conf.high, fill=group), alpha=0.15)+ #confidence interval
+  scale_linetype_manual(values = c("solid", "solid")) +
+  scale_y_continuous(limits = c(0,5)) + 
+  theme(legend.title = element_blank(), 
+        axis.title = element_text(size = 14),
+        plot.title = element_text(size = 14, vjust = +2),
+        axis.title.x = element_text(size = 12), 
+        axis.title.y = element_text(size = 12),) +
+  xlab("Left Ventricular Wall Thickness")+
+  ylab("Predicted values of CDR-SB") +
+  ggtitle("C)")
+
+p3
 
 # Education 
 
 df_lvh$education_deceased_cat <- ifelse(df_lvh$education_deceased >=4, 1,0) 
+df_lvh$education_deceased_cat <- as.factor(df_lvh$education_deceased_cat)
+levels(df_lvh$education_deceased_cat) <- c("<4 y", "≥4 y")
 
 m3 <- lm(cdr_sb ~ left_ventr_wall_thickness*education_deceased_cat
          + age
@@ -326,16 +443,46 @@ m3 <- lm(cdr_sb ~ left_ventr_wall_thickness*education_deceased_cat
 summary(m3)
 hist(m3$residuals)
 confint(m3)
+car::Anova(m3, type = "III")
 
-# Analysis with standardized variables ####
+plot_model(m3, type = "pred", terms = c("left_ventr_wall_thickness", "education_deceased_cat"), 
+           axis.title = c("Left Ventricular Wall Thickness", "CDR-SB"), 
+           title = "Predicted values of CDR-SB",  
+           legend.title = " ", 
+           ci.lvl = NA, #ci.lvl = confidence interval - default is 89%
+)
 
-#The scale() function can be used to scale variables in any arbitrary way, 
-#but the default is to standardize them.
+#2. Plotting the interaction with ggplot
 
-#each observation is subtracted from the mean and divided by the std deviation of the mean
+if(!require("ggeffects")) install.packages("ggeffects")
 
-scale(df_lvh$left_ventr_wall_thickness)
+df <- ggpredict(m3, terms = c("left_ventr_wall_thickness", "education_deceased_cat"))
+p4 <- 
+  ggplot(df, aes(x, predicted)) +
+  theme_classic()+
+  #theme(panel.grid.major.y = element_line()) + #to add horizontal lines on the background
+  geom_line(aes(linetype=group, color=group)) + 
+  geom_ribbon(aes(ymin=conf.low, ymax=conf.high, fill=group), alpha=0.15)+ #confidence interval
+  scale_linetype_manual(values = c("solid", "solid")) +
+  scale_y_continuous(limits = c(0,5)) + 
+  theme(legend.title = element_blank(), 
+        axis.title = element_text(size = 14),
+        plot.title = element_text(size = 14, vjust = +2),
+        axis.title.x = element_text(size = 12), 
+        axis.title.y = element_text(size = 12),) +
+  xlab("Left Ventricular Wall Thickness")+
+  ylab("Predicted values of CDR-SB") +
+  ggtitle("D)")
 
+p4
+
+
+#Arranging the four ggplots on the same page
+
+install.packages('patchwork')
+library(patchwork)
+
+p1 + p2 + p3 + p4 + plot_layout(nrow=2)
 ## Linear regressions ####
 
 #The output shows that a one standard deviation increase in left_ventr_wall_thickness
